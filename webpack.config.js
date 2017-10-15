fdecbef2d0e8ab0cd744a588505d9a627698d66c
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2017-10-15 16:02:18 -0300

    Add fetch polyfill to get the app working in 3 major browsers, ie, chrome, firefox.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: ['babel-polyfill', './src/index.jsx'],
+  entry: ['babel-polyfill', 'isomorphic-fetch', './src/index.jsx'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
